79704ccb72c67c98c49538a799952e5e6a259b35
ruachang <737211920@qq.com>
2024-05-11 16:10:02 -0500

    ?

--- requirement.sh
+++ requirement.sh
@@ -2,7 +2,7 @@ conda create --name img2txt python=3.10 -y
 conda activate img2txt
 
 # install packages
-pip install torch==2.0.0+cu117 torchvision==0.15.1+cu117 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu117
+pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu117
 pip install pytorch-lightning==1.5.0
 pip install opencv-python==4.7.0.72
 pip install matplotlib
